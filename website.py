9507d3e8b40f3bf28d9cd071f93ae2cb3dc2a0c5
Aaron <CheungA4@cardiff.ac.uk>
2024-04-12 18:47:06 +0100

    Added buttons redirecting to two pages on the home page, added text on home page, navbar redirects to home page

--- website.py
+++ website.py
@@ -6,5 +6,13 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def home():
     return render_template('index.html')
 
+@app.route('/machine_learning_models')
+def machine_learning():
+    return render_template('machinelearning.html')
+
+@app.route('/sentiment_analysis')
+def sentiment_analysis():
+    return render_template('sentimentanalysis.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
